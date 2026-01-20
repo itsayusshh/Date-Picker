bbf0d7cc651118514bfbed0dc23fc5a009248c51
Ayush Patel <ayush20122002@gmail.com>
2026-01-20 12:19:21 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { DateRangePicker } from "./components/DateRangePicker";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <DateRangePicker />
+    </div>
+  );
+}
+
+export default App;
